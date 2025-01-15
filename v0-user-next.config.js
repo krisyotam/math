13e5ee86e7ca30956f5a5dea5f1bf921c4fced06
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2025-01-15 05:51:51 -0600

    add jsdom

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -8,7 +8,12 @@ const nextConfig = {
     GHOST_API_URL: process.env.GHOST_API_URL,
     GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = ['jsdom', ...config.externals];
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
-
